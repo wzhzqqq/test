343e8e43978e07d8c8144a829016d636d3fbedb4
wzh <2064697749@qq.com>
2025-03-29 16:36:39 +0800

    main

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,10 @@
-print("helloworld!")
+import numpy as np
+import matplotlib.pyplot as plt
+def dwjy(t):
+    r=np.where(t>=0.0,1.0,0.0)
+    return r
+n=np.arange(-4,8)
+plt.ylim(0,2)
+plt.stem(n,dwjy(n))
+plt.show()
+
